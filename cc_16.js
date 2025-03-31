99dc26130a4a33e0bb4045320cde354cff362a61
Tyler Paiewonsky <tylerpsky@gmail.com>
2025-03-30 21:23:50 -0400

    Add fetchProductsAsync using async/await and try/catch

--- cc_16.js
+++ cc_16.js
@@ -1,3 +1,5 @@
+//Task 2
+
 function fetchProductsThen() {
     fetch('https://www.course-api.com/javascript-store-products')
         .then(response => {
@@ -15,4 +17,27 @@ function fetchProductsThen() {
 }
 
 fetchProductsThen();
+
+//Task 3
+
+async function fetchProductsAsync() {
+    try {
+        const response = await fetch('https://api.example.com/products'); 
+        if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        const products = await response.json();
+        displayProducts(products);
+    } catch (error) {
+        handleError(error);
+    }
+}
+
+function displayProducts(products) {
+    console.log("Displaying products:", products);
+}
+
+function handleError(error) {
+    console.error("Error fetching products:", error);
+}
 
